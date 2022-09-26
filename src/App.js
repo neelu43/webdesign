329f41d4bffac501dc6d13c6be5e2e2456fd08bb
vedika90 <neeluchandra43@gmail.com>
2022-09-26 17:37:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import About from "./components/About";
+// import Home from "./components/Home.jsx";
+// import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+     
+    
+    <About/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './about.css';
+import pic from '../assets/neel.PNG';
+const About = () => {
+  return (
+    <>
+    <div className='main'>
+        <div className="left">
+<img src={pic} alt=''/>
+        </div>
+        <div className="right">
+            <div className='one'>
+                <div className='oone'><h3>Download My Book</h3></div>
+                <div className='ttwo'><h3>15 MoneySaving Ideas</h3></div>
+                <div className='three'><h3>Future Proofing Your Career</h3></div>
+            </div>
+            <div className='two'>
+                <div className='four'><h3>10 Online Side Hustles</h3></div>
+                <div className='five'><h3>How To Be A Good Mentor</h3></div>
+                <div className='six'><h3>40 SimpleSelf Care Tips</h3></div>
+            </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default About
